90d4c862de4a15b5d44d2433f85e4ff1b8c7c3a9
Nicolas Payot <nicolas.payot@gmail.com>
2018-03-11 17:27:54 +0100

    Add exercises for control structures

---
+++ 2-control-structures/main.js
@@ -0,0 +1,22 @@
+const greaterNum = (a, b) => {
+  // TODO
+};
+
+const helloWorld = lang => {
+  // TODO
+};
+
+const grade = score => {
+  // TODO
+};
+
+const sort = (a, b, c) => {
+  // TODO
+}
+
+module.exports = {
+  greaterNum,
+  helloWorld,
+  grade,
+  sort
+};
