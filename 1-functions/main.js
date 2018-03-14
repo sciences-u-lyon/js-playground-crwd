4d78342ce07c13f600c3203ebbd2f052c2444401
Nicolas Payot <nicolas.payot@gmail.com>
2018-03-14 09:09:54 +0100

    Add instructions for functions

--- 1-functions/main.js
+++ 1-functions/main.js
@@ -1,23 +1,43 @@
+/**
+ * This function should return "HELLO" if "hello" was passed as an argument.
+ */
 const uppercase = str => {
   // TODO
 };
 
+/**
+ * This function should return "Hello has 5 characters" if "Hello" was passed as an argument.
+ */
 const charCount = str => {
   // TODO
 };
 
+/**
+ * This function should return "Hello Alice!" if "Alice" was passed as an argument.
+ * It should return "Hello John!" if no argument was passed.
+ * You can't use if / else statements
+ */
 const greetings = name => {
   // TODO
 };
 
+/**
+ * This function should return the area of a rectangle, given its width and height.
+ */
 const rectangleArea = (width, height) => {
   // TODO
 };
 
+/**
+ * This function should return the area of a circle, given its radius.
+ */
 const circleArea = radius => {
   // TODO
 };
 
+/**
+ * This function should return the volume of a sphere, given its radius.
+ */
 const sphereVolume = radius => {
   // TODO
 };
