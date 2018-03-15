2da9e185f322bbe97e438f2edfadee1c35d9f57b
Nicolas Payot <nicolas.payot@gmail.com>
2018-03-15 09:12:05 +0100

    Add instructions for control structures

--- 2-control-structures/main.js
+++ 2-control-structures/main.js
@@ -1,23 +1,50 @@
+/**
+ * This function should return the greater number between a and b
+ */
 const greaterNum = (a, b) => {
   // TODO
 };
 
+/**
+ * This function should return "Hello World" translated in a given language code:
+ * - fr (french)
+ * - es (spanish)
+ * - en (english)
+ * It should return "Hello World" (english) if no language was passed as an argument
+ */
 const helloWorld = lang => {
   // TODO
 };
 
+/**
+ * This function should return a grade between 'A' and 'E' according to a given score:
+ * - A: score between 20 and 17
+ * - B: score between 16 and 13
+ * - C: score between 12 and 9
+ * - D: score between 8 and 4
+ * - E: score between 3 and 0
+ */
 const grade = score => {
   // TODO
 };
 
+/**
+ * This function should return the three given numbers a, b and c as a string, in ASC order
+ */
 const sort = (a, b, c) => {
   // TODO
 };
 
+/**
+ * This function should return the sum of a given list of numbers
+ */
 const sum = numbers => {
   // TODO
 };
 
+/**
+ * This function should only return the given names that have more than 3 characters, as a string
+ */
 const filterNames = names => {
   // TODO
 };
