dc7d0940729f403c3883af60d3745abe747d1b96
Nicolas Payot <nicolas.payot@gmail.com>
2018-03-19 19:14:00 +0100

    Add exercises for arrays

---
+++ 3-arrays/main.js
@@ -0,0 +1,41 @@
+/**
+ * This function should return a list of tv shows titles
+ * - for loop is forbidden
+ * - forEach is forbidden
+ */
+const tvShowsTitles = tvShows => {
+  // TODO
+};
+
+/**
+ * This function should return a list of the tv shows where rate is greater than the given rate
+ * - for loop is forbidden
+ * - forEach is forbidden
+ */
+const greatTVShows = (tvShows, rate) => {
+  // TODO
+};
+
+/**
+ * This function should return the average rate of the list of tv shows
+ * - for loop is forbidden
+ * - forEach is forbidden
+ */
+const tvShowsAverageRate = tvShows => {
+  // TODO
+};
+
+/**
+ * This function should return a list of the tv shows sorted by a given key (title or rate)
+ */
+const tvShowsSortedBy = (tvShows, key) => {
+  // TODO
+};
+
+module.exports = {
+  tvShowsTitles,
+  greatTVShows,
+  tvShowsAverageRate,
+  tvShowsSortedBy
+};
+
---
+++ 3-arrays/main.test.js
@@ -0,0 +1,77 @@
+const main = require('./main');
+
+describe('3-arrays', () => {
+  let tvShows;
+
+  beforeEach(() => {
+    tvShows = [
+      { title: 'Black Mirror', rate: 96 },
+      { title: 'Breaking Bad', rate: 95 },
+      { title: 'Game Of Thrones', rate: 94 },
+      { title: 'Jessica Jones', rate: 92 },
+      { title: 'Stranger Things', rate: 94 },
+      { title: 'The Walking Dead', rate: 81 }
+    ];
+  });
+
+  it('should return tv shows titles', () => {
+    expect(main.tvShowsTitles(tvShows)).toEqual([
+      'BLACK MIRROR',
+      'BREAKING BAD',
+      'GAME OF THRONES',
+      'JESSICA JONES',
+      'STRANGER THINGS',
+      'THE WALKING DEAD'
+    ]);
+  });
+
+  xit('should return tv shows with rate greater than 93', () => {
+    expect(main.greatTVShows(tvShows, 93)).toEqual([
+      { title: 'Black Mirror', rate: 96 },
+      { title: 'Breaking Bad', rate: 95 },
+      { title: 'Game Of Thrones', rate: 94 },
+      { title: 'Stranger Things', rate: 94 }
+    ]);
+  });
+
+  xit('should return tv shows average rate', () => {
+    expect(main.tvShowsAverageRate(tvShows)).toBe(92);
+  });
+
+  describe('tvShowsSortedBy', () => {
+    let unsortedTVShows;
+
+    beforeEach(() => {
+      unsortedTVShows = [
+        { title: 'Game Of Thrones', rate: 94 },
+        { title: 'Black Mirror', rate: 96 },
+        { title: 'Stranger Things', rate: 94 },
+        { title: 'Jessica Jones', rate: 92 },
+        { title: 'Breaking Bad', rate: 95 },
+        { title: 'The Walking Dead', rate: 81 }
+      ];
+    });
+
+    xit('should return tv shows sorted by title', () => {
+      expect(main.tvShowsSortedBy(unsortedTVShows, 'title')).toEqual([
+        { title: 'Black Mirror', rate: 96 },
+        { title: 'Breaking Bad', rate: 95 },
+        { title: 'Game Of Thrones', rate: 94 },
+        { title: 'Jessica Jones', rate: 92 },
+        { title: 'Stranger Things', rate: 94 },
+        { title: 'The Walking Dead', rate: 81 }
+      ]);
+    });
+
+    xit('should return tv shows sorted by rate', () => {
+      expect(main.tvShowsSortedBy(unsortedTVShows, 'rate')).toEqual([
+        { title: 'The Walking Dead', rate: 81 },
+        { title: 'Jessica Jones', rate: 92 },
+        { title: 'Game Of Thrones', rate: 94 },
+        { title: 'Stranger Things', rate: 94 },
+        { title: 'Breaking Bad', rate: 95 },
+        { title: 'Black Mirror', rate: 96 }
+      ]);
+    });
+  });
+});
